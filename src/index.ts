6e118ebf334841f117a217ca4f2a0e32889b3c79
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-02-08 11:32:53 +0700

    remove ramda

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { writeFileSync } from "fs";
+import { format } from "prettier";
+import { renderToStaticMarkup } from "react-dom/server";
+import { App } from "./app.js";
+
+const html = renderToStaticMarkup(App());
+const addDoctype = "<!DOCTYPE html>" + html;
+const formatDoc = format(addDoctype, { parser: "html" });
+const writeDoc = writeFileSync("./public/index.html", formatDoc);
+
+console.log(writeDoc, `File Created`);
